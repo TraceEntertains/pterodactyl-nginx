04b5c7580bad897d18d24c09696c47b9d766c5b5
TraceEntertains <73800394+TraceEntertains@users.noreply.github.com>
2022-06-04 22:30:06 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-ls
-
 echo "Starting PHP-FPM..."
-/php-fpm7 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
+/sbin/php-fpm7 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
 echo "Starting Nginx..."
-/nginx -c /home/container/nginx/nginx.conf
+/sbin/nginx -c /home/container/nginx/nginx.conf
