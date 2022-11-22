66d0ae62b22faea683f98cf9d6866a8a72bd9ecc
TraceEntertains <73800394+TraceEntertains@users.noreply.github.com>
2022-11-22 11:30:27 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-certbot certonly --nginx -d ${TARGET_DOMAIN}
+echo "Updating certificates..."
+sudo certbot certonly --nginx -d ${TARGET_DOMAIN}
 
 echo "Starting PHP-FPM..."
 sudo /usr/sbin/php-fpm7.0 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
