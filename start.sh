69d7b1da2e60a04ed4a108c5538980cc3a6eedcc
TraceEntertains <73800394+TraceEntertains@users.noreply.github.com>
2022-11-22 12:31:07 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Updating certificates..."
-sudo certbot certonly --nginx -d ${TARGET_DOMAIN}
+certbot certonly --nginx -d ${TARGET_DOMAIN}
 
 echo "Starting PHP-FPM..."
-sudo /usr/sbin/php-fpm7.0 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
+/usr/sbin/php-fpm7.0 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
 echo "Starting Nginx..."
-sudo /sbin/nginx -c /home/container/nginx/nginx.conf
+/sbin/nginx -c /home/container/nginx/nginx.conf
