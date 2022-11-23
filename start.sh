6c096aea6ab28c30b7bedb83d0d45817874e5aaa
TraceEntertains <73800394+TraceEntertains@users.noreply.github.com>
2022-11-22 19:04:18 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo "Updating certificates..."
-certbot certonly --test-cert --manual --preferred-challenges dns -d ${TARGET_DOMAIN} --config-dir /home/container/certconfig --work-dir /home/container/certs --logs-dir /home/container/logs
+certbot certonly --keep-until-expiring --manual --preferred-challenges dns -d ${TARGET_DOMAIN} --config-dir /home/container/certconfig --work-dir /home/container/certs --logs-dir /home/container/logs
+chmod 755 /home/container/certconfig/live/${TARGET_DOMAIN}/cert.pem
+chmod 755 /home/container/certconfig/live/${TARGET_DOMAIN}/privkey.pem
 
 echo "Starting PHP-FPM..."
 /usr/sbin/php-fpm7.0 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
